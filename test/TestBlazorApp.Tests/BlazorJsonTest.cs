d7b9a6d4bc55bea868be5dd206680bc8ec1eb6c7
Beffyman <splashy5@hotmail.com>
2018-07-07 00:20:47 -0400

    added blazor package & basic test (#8)

---
+++ test/TestBlazorApp.Tests/BlazorJsonTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using TestBlazorApp.Clients;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace TestBlazorApp.Tests
+{
+	[TestFixture]
+	public class BlazorJsonTest
+	{
+		[Test]
+		public void WeatherForecastsTest()
+		{
+			var endpoint = new BlazorJsonServerInfo();
+
+			var sampleDataClient = endpoint.Provider.GetService<ISampleDataClient>();
+			var forecasts = sampleDataClient.WeatherForecasts();
+
+
+			Assert.IsTrue(forecasts.Count() == 5);
+		}
+	}
+}
---
+++ test/TestBlazorApp.Tests/ServerInfo.cs
@@ -0,0 +1,45 @@
+﻿using AspNetCore.Client.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using TestBlazorApp.Clients;
+using TestBlazorApp.Server;
+
+namespace TestBlazorApp.Tests
+{
+	public abstract class ServerInfo<T> where T : class
+	{
+		public IServiceProvider Provider { get; }
+		public TestServer Server { get; }
+		public HttpClient Client { get; }
+
+		public ServerInfo()
+		{
+			Server = new TestServer(new WebHostBuilder()
+					.UseStartup<T>());
+
+			Client = Server.CreateClient();
+
+			var services = new ServiceCollection();
+			services.AddSingleton(Client);
+			services.InstallClients(ConfigureClient);
+
+			Provider = services.BuildServiceProvider();
+		}
+
+		protected abstract void ConfigureClient(ClientConfiguration configure);
+	}
+
+
+	public class BlazorJsonServerInfo : ServerInfo<Startup>
+	{
+		protected override void ConfigureClient(ClientConfiguration configure)
+		{
+			configure.UseBlazorSimpleJsonSerlaizer();
+		}
+	}
+}
