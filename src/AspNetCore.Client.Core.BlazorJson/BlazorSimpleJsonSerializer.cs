999b6b598c6e12b832987b5c966316389c4a8898
Connor Laderer <splashy5@hotmail.com>
2018-07-07 00:12:45 -0400

    added blazor package & basic test

---
+++ src/AspNetCore.Client.Core.BlazorJson/BlazorSimpleJsonSerializer.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCore.Client.Core.Serializers;
+using System;
+using Microsoft.AspNetCore.Blazor;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Client.Core.Serializers
+{
+	/// <summary>
+	/// Uses Blazor's SimpleJson for serializing and deserializing the http content
+	/// </summary>
+	public class BlazorSimpleJsonSerializer : IHttpSerializer
+	{
+		public async ValueTask<T> Deserialize<T>(HttpContent content)
+		{
+			return JsonUtil.Deserialize<T>(await content.ReadAsStringAsync().ConfigureAwait(false));
+		}
+
+		public string Serialize<T>(T request)
+		{
+			return JsonUtil.Serialize(request);
+		}
+	}
+}
---
+++ src/AspNetCore.Client.Core.BlazorJson/Installer.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.Client.Core.Serializers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Client.Core
+{
+	public static class ProtobufInstaller
+	{
+
+		public static void UseBlazorSimpleJsonSerlaizer(this ClientConfiguration config)
+		{
+			config.SerializeType = typeof(BlazorSimpleJsonSerializer);
+		}
+	}
+}
