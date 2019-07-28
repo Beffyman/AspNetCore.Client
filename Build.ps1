69249a2e79b0b3ec6af60c6ee57a367796cacb17
Connor Laderer <splashy5@hotmail.com>
2019-07-28 11:35:14 -0400

    Revert "add env vars"
    This reverts commit bc399a25096e199d142ccba7f31b625f50415b9f.

--- Build.ps1
+++ Build.ps1
@@ -6,10 +6,7 @@ Param(
 
 Write-Output "Windows PowerShell $($Host.Version)"
 
-Set-StrictMode -Version 2.0;
-$ErrorActionPreference = "Stop";
-$ConfirmPreference = "None";
-trap { Write-Host $_; exit 1 }
+Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 ###########################################################################
@@ -30,45 +27,6 @@ $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 # EXECUTION
 ###########################################################################
 
-function HasDotnetVersion{
-	Param(
-		[string]$expectedVersion
-	)
-
-	$sdks = @();
-
-	((dotnet --list-sdks).Split([System.Environment]::NewLine) `
-		| % {$sdks += $_.Split(" [")[0].Trim()});
-
-	$expectedMajorVersion = [int]($expectedVersion.Split(".")[0]);
-	$expectedMinorVersion = [int]($expectedVersion.Split(".")[1]);
-	$expectedPatchVersionFirst = [int]::Parse(($expectedVersion.Split(".")[2][0]));
-	$expectedPatchVersion = [int]($expectedVersion.Split(".")[2]);
-
-	foreach($sdk in $sdks){
-		try{
-			$sdkMajorVersion = [int]($sdk.Split(".")[0]);
-			$sdkMinorVersion = [int]($sdk.Split(".")[1]);
-			$sdkPatchVersionFirst = [int]::Parse(($sdk.Split(".")[2][0]));
-			$sdkPatchVersion = [int]($sdk.Split(".")[2]);
-
-			if($expectedMajorVersion -eq $sdkMajorVersion){
-				if($expectedMinorVersion -eq $sdkMinorVersion){
-					if($expectedPatchVersionFirst -eq $sdkPatchVersionFirst){
-						if($expectedPatchVersion -le $sdkPatchVersion){
-							Write-Host "$sdk is installed" -ForegroundColor Magenta;
-							return $true;
-						}
-					}
-				}
-			}
-
-		}catch{}
-	}
-
-	return $false;
-}
-
 function ExecSafe([scriptblock] $cmd) {
     & $cmd
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
@@ -76,38 +34,32 @@ function ExecSafe([scriptblock] $cmd) {
 
 # If global.json exists, load expected version
 if (Test-Path $DotNetGlobalFile) {
-	$DotNetGlobal = $(Get-Content $DotNetGlobalFile | Out-String | ConvertFrom-Json)
-	if ($DotNetGlobal.PSObject.Properties["sdk"] -and $DotNetGlobal.sdk.PSObject.Properties["version"]) {
-		$DotNetVersion = $DotNetGlobal.sdk.version
-	}
+    $DotNetGlobal = $(Get-Content $DotNetGlobalFile | Out-String | ConvertFrom-Json)
+    if ($DotNetGlobal.PSObject.Properties["sdk"] -and $DotNetGlobal.sdk.PSObject.Properties["version"]) {
+        $DotNetVersion = $DotNetGlobal.sdk.version
+    }
 }
 
 # If dotnet is installed locally, and expected version is not set or installation matches the expected version
 if ((Get-Command "dotnet" -ErrorAction SilentlyContinue) -ne $null -and `
-	$env:ForceSdkDownload -ne $true -and `
-     (!(Test-Path variable:DotNetVersion) -or (HasDotnetVersion -expectedVersion $DotNetVersion -eq $true))) {
-	$env:DOTNET_EXE = (Get-Command "dotnet").Path
+     (!(Test-Path variable:DotNetVersion) -or $(& dotnet --version) -eq $DotNetVersion)) {
+    $env:DOTNET_EXE = (Get-Command "dotnet").Path
 }
 else {
-	$DotNetDirectory = "$TempDirectory\dotnet-win"
-	$env:DOTNET_EXE = "$DotNetDirectory\dotnet.exe"
+    $DotNetDirectory = "$TempDirectory\dotnet-win"
+    $env:DOTNET_EXE = "$DotNetDirectory\dotnet.exe"
 
-	# Download install script
-	$DotNetInstallFile = "$TempDirectory\dotnet-install.ps1"
-	md -force $TempDirectory > $null
+    # Download install script
+    $DotNetInstallFile = "$TempDirectory\dotnet-install.ps1"
+    md -force $TempDirectory > $null
+    (New-Object System.Net.WebClient).DownloadFile($DotNetInstallUrl, $DotNetInstallFile)
 
-	try{
-		(New-Object System.Net.WebClient).DownloadFile($DotNetInstallUrl, $DotNetInstallFile)
-	}catch{
-		throw "There was an issue downloading the dotnet sdk for the tmp dir.  There may be a proxy blocking the sdk download.";
-	}
-
-	# Install by channel or version
-	if (!(Test-Path variable:DotNetVersion)) {
-		ExecSafe { & $DotNetInstallFile -InstallDir $DotNetDirectory -Channel $DotNetChannel -NoPath }
-	} else {
-		ExecSafe { & $DotNetInstallFile -InstallDir $DotNetDirectory -Version $DotNetVersion -NoPath }
-	}
+    # Install by channel or version
+    if (!(Test-Path variable:DotNetVersion)) {
+        ExecSafe { & $DotNetInstallFile -InstallDir $DotNetDirectory -Channel $DotNetChannel -NoPath }
+    } else {
+        ExecSafe { & $DotNetInstallFile -InstallDir $DotNetDirectory -Version $DotNetVersion -NoPath }
+    }
 }
 
 Write-Output "Microsoft (R) .NET Core SDK version $(& $env:DOTNET_EXE --version)"
