a7e0af759f6bdd0da9ac99a7760377b9d1fcac08
Connor Laderer <splashy5@hotmail.com>
2020-01-17 18:01:23 -0500

    see if generator is getting locked up on web app?

--- tests/TestWebApp.Tests/SignalRTests.cs
+++ tests/TestWebApp.Tests/SignalRTests.cs
@@ -1,174 +1,174 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using Microsoft.Extensions.DependencyInjection;
-//using TestWebApp.Contracts;
-//using TestWebApp.Hubs;
-//using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TestWebApp.Contracts;
+using TestWebApp.Hubs;
+using Xunit;
 
-//namespace TestWebApp.Tests
-//{
-//	public static class CancellationTokenHelper
-//	{
-//		public static Task WhenCanceled(this CancellationToken cancellationToken)
-//		{
-//			var tcs = new TaskCompletionSource<bool>();
-//			cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
-//			return tcs.Task;
-//		}
-//	}
+namespace TestWebApp.Tests
+{
+	public static class CancellationTokenHelper
+	{
+		public static Task WhenCanceled(this CancellationToken cancellationToken)
+		{
+			var tcs = new TaskCompletionSource<bool>();
+			cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+			return tcs.Task;
+		}
+	}
 
-//	public class SignalRTests
-//	{
+	public class SignalRTests
+	{
 
-//		[Fact]
-//		public async Task SendReceiveMessageAsync()
-//		{
-//			using (var endpoint = new JsonServerInfo())
-//			{
-//				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
-//					config =>
-//					{
-//						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
-//					})
-//					.Build();
+		[Fact]
+		public async Task SendReceiveMessageAsync()
+		{
+			using (var endpoint = new JsonServerInfo())
+			{
+				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
+					config =>
+					{
+						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
+					})
+					.Build();
 
-//				string user = null;
-//				string message = null;
+				string user = null;
+				string message = null;
 
-//				CancellationTokenSource tokenSource = new CancellationTokenSource(2000);
-//				var token = tokenSource.Token;
+				CancellationTokenSource tokenSource = new CancellationTokenSource(2000);
+				var token = tokenSource.Token;
 
-//				hub.OnReceiveMessage((usr, msg) =>
-//				{
-//					user = usr;
-//					message = msg;
-//					tokenSource.Cancel();
-//				});
+				hub.OnReceiveMessage((usr, msg) =>
+				{
+					user = usr;
+					message = msg;
+					tokenSource.Cancel();
+				});
 
-//				await hub.StartAsync();
+				await hub.StartAsync();
 
-//				await hub.SendMessageAsync("Test", "Hello World");
+				await hub.SendMessageAsync("Test", "Hello World");
 
-//				await token.WhenCanceled();
+				await token.WhenCanceled();
 
-//				await hub.StopAsync();
+				await hub.StopAsync();
 
-//				Assert.Equal("Test", user);
-//				Assert.Equal("Hello World", message);
-//			}
-//		}
-
-
-//		[Fact]
-//		public async Task CounterChannelTest()
-//		{
-//			using (var endpoint = new JsonServerInfo())
-//			{
-//				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
-//					config =>
-//					{
-//						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
-//					})
-//					.Build();
-
-//				int count = 100;
-//				int delay = 20;
-
-//				IList<int> results = new List<int>();
-
-//				await hub.StartAsync();
-
-//				var channel = await hub.StreamCounterAsync(count, delay);
-
-//				while (await channel.WaitToReadAsync())
-//				{
-//					while (channel.TryRead(out int item))
-//					{
-//						results.Add(item);
-//					}
-//				}
-
-//				await hub.StopAsync();
-
-//				Assert.Equal(count, results.Count());
-//			}
-//		}
+				Assert.Equal("Test", user);
+				Assert.Equal("Hello World", message);
+			}
+		}
 
 
-//		[Fact]
-//		public async Task CounterBlockingTest()
-//		{
-//			using (var endpoint = new JsonServerInfo())
-//			{
-//				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
-//					config =>
-//					{
-//						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
-//					})
-//					.Build();
+		[Fact]
+		public async Task CounterChannelTest()
+		{
+			using (var endpoint = new JsonServerInfo())
+			{
+				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
+					config =>
+					{
+						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
+					})
+					.Build();
 
-//				int count = 100;
-//				int delay = 20;
+				int count = 100;
+				int delay = 20;
 
-//				await hub.StartAsync();
+				IList<int> results = new List<int>();
 
-//				IEnumerable<int> results = await hub.ReadCounterBlockingAsync(count, delay);
+				await hub.StartAsync();
 
-//				await hub.StopAsync();
+				var channel = await hub.StreamCounterAsync(count, delay);
 
-//				Assert.Equal(count, results.Count());
-//			}
-//		}
+				while (await channel.WaitToReadAsync())
+				{
+					while (channel.TryRead(out int item))
+					{
+						results.Add(item);
+					}
+				}
+
+				await hub.StopAsync();
+
+				Assert.Equal(count, results.Count());
+			}
+		}
 
 
-//		[Fact]
-//		public async Task MessagePackTest()
-//		{
-//			using (var endpoint = new MessagePackServerInfo())
-//			{
-//				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
-//					config =>
-//					{
-//						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
-//					})
-//					.AddMessagePackProtocol()
-//					.Build();
+		[Fact]
+		public async Task CounterBlockingTest()
+		{
+			using (var endpoint = new JsonServerInfo())
+			{
+				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
+					config =>
+					{
+						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
+					})
+					.Build();
 
-//				MyFancyDto actual = null;
-//				MyFancyDto expected = new MyFancyDto
-//				{
-//					Collision = Guid.NewGuid(),
-//					Description = "I am a test",
-//					Id = 15,
-//					When = DateTime.Now
-//				};
+				int count = 100;
+				int delay = 20;
 
-//				CancellationTokenSource tokenSource = new CancellationTokenSource(2000);
-//				var token = tokenSource.Token;
+				await hub.StartAsync();
 
-//				hub.OnReceiveMessage2((dto) =>
-//				{
-//					actual = dto;
-//					tokenSource.Cancel();
-//				});
+				IEnumerable<int> results = await hub.ReadCounterBlockingAsync(count, delay);
 
-//				await hub.StartAsync();
+				await hub.StopAsync();
 
-//				await hub.DtoMessageAsync(expected);
+				Assert.Equal(count, results.Count());
+			}
+		}
 
-//				await token.WhenCanceled();
 
-//				await hub.StopAsync();
+		[Fact]
+		public async Task MessagePackTest()
+		{
+			using (var endpoint = new MessagePackServerInfo())
+			{
+				var hub = new ChatHubConnectionBuilder(endpoint.Server.BaseAddress, null,
+					config =>
+					{
+						config.HttpMessageHandlerFactory = _ => endpoint.Server.CreateHandler();
+					})
+					.AddMessagePackProtocol()
+					.Build();
 
-//				Assert.Equal(expected.Collision, actual.Collision);
-//				Assert.Equal(expected.Description, actual.Description);
-//				Assert.Equal(expected.Id, actual.Id);
-//				Assert.Equal(expected.When.ToLocalTime(), actual.When.ToLocalTime());
-//			}
-//		}
+				MyFancyDto actual = null;
+				MyFancyDto expected = new MyFancyDto
+				{
+					Collision = Guid.NewGuid(),
+					Description = "I am a test",
+					Id = 15,
+					When = DateTime.Now
+				};
 
-//	}
-//}
+				CancellationTokenSource tokenSource = new CancellationTokenSource(2000);
+				var token = tokenSource.Token;
+
+				hub.OnReceiveMessage2((dto) =>
+				{
+					actual = dto;
+					tokenSource.Cancel();
+				});
+
+				await hub.StartAsync();
+
+				await hub.DtoMessageAsync(expected);
+
+				await token.WhenCanceled();
+
+				await hub.StopAsync();
+
+				Assert.Equal(expected.Collision, actual.Collision);
+				Assert.Equal(expected.Description, actual.Description);
+				Assert.Equal(expected.Id, actual.Id);
+				Assert.Equal(expected.When.ToLocalTime(), actual.When.ToLocalTime());
+			}
+		}
+
+	}
+}
