d0fc26f5d685dc4f05ff786649318bcb39ef30ec
Connor Laderer <splashy5@hotmail.com>
2020-01-20 13:19:03 -0500

    signalR doesn't support tokens in method params
    https://github.com/aspnet/SignalR/issues/2241#issue-321758761

--- tests/TestWebApp/Hubs/ChatHub.cs
+++ tests/TestWebApp/Hubs/ChatHub.cs
@@ -18,9 +18,9 @@ namespace TestWebApp.Hubs
 		}
 
 		[ProducesMessage("ReceiveMessage", typeof(string), typeof(string))]
-		public async Task SendMessage(string user, string message, CancellationToken token = default)
+		public async Task SendMessage(string user, string message)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", user, message, token);
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
 		}
 
 		[ProducesMessage("ReceiveMessage2", typeof(MyFancyDto))]
